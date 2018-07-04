00f1a056abee5e2fe9628a4be05be1824607e565
Dave Voorhis <dave@armchair.mb.ca>
2018-07-04 19:47:44 +0100

    Work on Shell handling.

--- Reli/src/org/reldb/relang/reli/RelI.java
+++ Reli/src/org/reldb/relang/reli/RelI.java
@@ -10,7 +10,79 @@ import org.reldb.swt.os_specific.OSSpecific;
 public class RelI {
 
 	static boolean createdScreenBar = false;
+	
+	static void createFileMenu(Menu bar) {
+		
+		MenuItem fileItem = new MenuItem(bar, SWT.CASCADE);			
+		fileItem.setText("File");
+		
+		Menu menu = new Menu(fileItem);
+		fileItem.setMenu(menu);
 
+		MenuItem newDatasheet = new MenuItem(menu, SWT.PUSH);
+		newDatasheet.setText("New");
+		newDatasheet.setAccelerator(SWT.MOD1 | 'N');
+		newDatasheet.addListener(SWT.Selection, event -> {
+			Shell s1 = createShell();
+			s1.open();
+		});
+		
+		MenuItem openDatasheet = new MenuItem(menu, SWT.PUSH);
+		openDatasheet.setText("Open Datasheet...");
+		openDatasheet.setAccelerator(SWT.MOD1 | 'O');
+		openDatasheet.addListener(SWT.Selection, event -> {
+			
+		});
+		
+		if (!SWT.getPlatform().equals("cocoa")) {
+			MenuItem exit = new MenuItem(menu, SWT.PUSH);
+			exit.setText("Exit");
+			exit.addListener(SWT.Selection, event -> {
+				Display d = Display.getCurrent();
+				Shell[] shells = d.getShells();
+				for (Shell shell : shells) {
+					shell.close();
+				}
+			});
+		}
+		
+	}
+	
+	static void createEditMenu(Menu bar) {		
+		MenuItem editItem = new MenuItem(bar, SWT.CASCADE);
+		editItem.setText("Edit");
+	}
+
+	static void createDataMenu(Menu bar) {
+		
+		MenuItem dataItem = new MenuItem(bar, SWT.CASCADE);
+		dataItem.setText("Data");
+		
+		Menu menu = new Menu(dataItem);
+		dataItem.setMenu(menu);
+
+		MenuItem newGrid = new MenuItem(menu, SWT.PUSH);
+		newGrid.setText("New Grid...");
+		newGrid.setAccelerator(SWT.MOD1 | 'G');
+		newGrid.addListener(SWT.Selection, event -> {
+			
+		});
+		
+		MenuItem loadFile = new MenuItem(menu, SWT.PUSH);
+		loadFile.setText("Load...");
+		loadFile.setAccelerator(SWT.MOD1 | 'L');
+		loadFile.addListener(SWT.Selection, event -> {
+			
+		});
+		
+		MenuItem importFile = new MenuItem(menu, SWT.PUSH);
+		importFile.setText("Import...");
+		importFile.setAccelerator(SWT.MOD1 | 'I');
+		importFile.addListener(SWT.Selection, event -> {
+			
+		});
+	}
+	
 	static void createMenuBar(Shell s) {
 		Menu bar = Display.getCurrent().getMenuBar();
 		boolean hasAppMenuBar = (bar != null);
@@ -22,30 +94,9 @@ public class RelI {
 		// Otherwise, we need to make a new menu bar for each shell.
 		if (!createdScreenBar || !hasAppMenuBar) {
 			
-			MenuItem item = new MenuItem(bar, SWT.CASCADE);			
-			item.setText("File");
-			Menu menu = new Menu(item);
-			item.setMenu(menu);
-
-			MenuItem newWindow = new MenuItem(menu, SWT.PUSH);
-			newWindow.setText("New Window");
-			newWindow.setAccelerator(SWT.MOD1 | 'N');
-			newWindow.addListener(SWT.Selection, event -> {
-				Shell s1 = createShell();
-				s1.open();
-			});
-			
-			if (!SWT.getPlatform().equals("cocoa")) {
-				MenuItem exit = new MenuItem(menu, SWT.PUSH);
-				exit.setText("Exit");
-				exit.addListener(SWT.Selection, event -> {
-					Display d = Display.getCurrent();
-					Shell[] shells = d.getShells();
-					for (Shell shell : shells) {
-						shell.close();
-					}
-				});
-			}
+			createFileMenu(bar);
+			createEditMenu(bar);
+			createDataMenu(bar);
 			
 			if (!hasAppMenuBar) 
 				s.setMenuBar(bar);
@@ -56,20 +107,24 @@ public class RelI {
 	static Shell createShell() {
 		final Shell shell = new Shell(SWT.SHELL_TRIM);
 		createMenuBar(shell);
-
+		
 		shell.addDisposeListener(e -> {
 			Display display = Display.getCurrent();
 			Menu bar = display.getMenuBar();
 			boolean hasAppMenuBar = (bar != null);
+			Shell[] shells = display.getShells();
 			if (!hasAppMenuBar && shell != null && shell.getMenuBar() != null) {
 				shell.getMenuBar().dispose();
-				Shell[] shells = display.getShells();
 				if ((shells.length == 1) && (shells[0] == shell)) {
 					if (!display.isDisposed()) 
 						display.dispose();
+					System.out.println("RelI last shell?");
 				}
 			}
+			System.out.println("RelI shell disposing. Shell count == " + shells.length);
 		});
+		
+		shell.addListener(SWT.Close, event -> System.out.println("RelI shell closing"));
 
 		return shell;
 	}
