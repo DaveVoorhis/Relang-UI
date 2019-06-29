8ee601912a09112a2708019acfd6f510bd128fe2
dave <dave@armchair.mb.ca>
2019-06-29 11:53:11 +0100

    Update-in-progress to JUnit5.

--- Tests/src/RelangTests.java
+++ Tests/src/RelangTests.java
@@ -7,6 +7,20 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.function.Executable;
+
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherFactory;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.junit.platform.launcher.listeners.TestExecutionSummary.Failure;
+
 public class RelangTests {
 	
 	// Array of packages containing tests.
@@ -92,6 +106,25 @@ public class RelangTests {
 		for (Class<?> klass: classes)
 			names.add(klass.getName());
 		return names;
+	}
+
+	public static void testClasses(Class<?>[] classes) {
+	    final LauncherDiscoveryRequest request = 
+	        LauncherDiscoveryRequestBuilder.request()
+	                                   .selectors(selectClass(MyClass.class))
+	                                   .build();
+
+	        final Launcher launcher = LauncherFactory.create();
+	        final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+	        launcher.registerTestExecutionListeners(listener);
+	        launcher.execute(request);
+
+	        TestExecutionSummary summary = listener.getSummary();
+	        long testFoundCount = summary.getTestsFoundCount();
+	        List<Failure> failures = summary.getFailures();
+	        System.out.println("getTestsSucceededCount() - " + summary.getTestsSucceededCount());
+	        failures.forEach(failure -> System.out.println("failure - " + failure.getException()));
 	}
 	
 	public static void main(String args[]) {
