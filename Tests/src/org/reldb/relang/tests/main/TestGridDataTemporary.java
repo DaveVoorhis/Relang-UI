2bff67a5dbff723c84a2003a34eeca39eb84c290
dave <dave@armchair.mb.ca>
2019-06-30 10:36:49 +0100

    Update to latest versions of various libraries.

--- Tests/src/org/reldb/relang/tests/main/TestGridDataTemporary.java
+++ Tests/src/org/reldb/relang/tests/main/TestGridDataTemporary.java
@@ -1,11 +1,10 @@
 package org.reldb.relang.tests.main;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.reldb.relang.core.GridData;
 import org.reldb.relang.core.GridDataTemporary;
-import org.reldb.relang.core.Heading;
 import org.reldb.relang.tests.BaseOfTest;
 
 public class TestGridDataTemporary extends BaseOfTest {
@@ -17,8 +16,6 @@ public class TestGridDataTemporary extends BaseOfTest {
 		data.setColumnName(1,  "Column2");
 		data.setColumnType(0, String.class, "");
 		data.setColumnType(1,  Integer.class, Integer.valueOf(0));
-		
-		// TODO fix broken!
 		data.setValue(0, 1, "blah");
 		data.setValue(1,  1, 22);
 	}
