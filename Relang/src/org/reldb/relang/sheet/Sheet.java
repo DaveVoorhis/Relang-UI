488925233a240bb256f24ac48af28500374c1af9
Dave Voorhis <dave@armchair.mb.ca>
2019-07-12 18:13:51 +0100

    Work on Sheet.

--- Relang/src/org/reldb/relang/sheet/Sheet.java
+++ Relang/src/org/reldb/relang/sheet/Sheet.java
@@ -1,15 +1,17 @@
 package org.reldb.relang.sheet;
 
 import org.eclipse.nebula.widgets.grid.GridColumn;
-import org.eclipse.nebula.widgets.grid.GridColumnGroup;
 import org.eclipse.nebula.widgets.grid.GridEditor;
 import org.eclipse.nebula.widgets.grid.GridItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.reldb.relang.data.Data;
 import org.reldb.relang.datagrid.Datagrid;
 import org.reldb.relang.datagrid.GridWidgetWrapper;
+import org.reldb.relang.utilities.DialogBase;
 
 public class Sheet {
 
@@ -18,17 +20,17 @@ public class Sheet {
 		var grid = new Datagrid(parent, SWT.BORDER | SWT.VIRTUAL | SWT.V_SCROLL | SWT.H_SCROLL);
 		
 		grid.getGrid().setHeaderVisible(true);
+//		grid.getGrid().get
 		
 		int columnCount = data.getColumnCount();
 		for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
-			var group = new GridColumnGroup(grid.getGrid(), SWT.NONE);
-			group.setText(data.getColumnTypeAt(columnIndex).getName());
-			var column = new GridColumn(group, SWT.NONE);
-			column.setSort(1);
+			var column = new GridColumn(grid.getGrid(), SWT.NONE);
+			column.setHeaderTooltip(data.getColumnTypeAt(columnIndex).getName());
 			column.setWidth(150);
 			column.setText(data.getColumnNameAt(columnIndex));
-			column.addListener(SWT.Selection, evt -> System.out.println("Sheet: Column selection listener triggered."));
-			column.addListener(SWT.MouseDown, evt -> System.out.println("Sheet: Column mousedown listener triggered."));
+			column.addListener(SWT.Selection, evt -> showColumnDialog(evt));
+			column.setWordWrap(true);
+			column.setMoveable(true);
 		}
 		
 		grid.getGrid().setLinesVisible(true);
@@ -56,5 +58,17 @@ public class Sheet {
 		});
 		
 		grid.focusOnCell(0, 0);		
+	}
+
+	private void showColumnDialog(Event evt) {
+		var column = (GridColumn)evt.item;
+		var parentShell = column.getParent().getShell();
+		var dialog = new DialogBase(parentShell, SWT.NONE) {
+			@Override
+			public void create(Shell shell) {
+				shell.setText("Properties for " + column.getText());
+			}
+		};
+		dialog.open();
 	}
 }
---
+++ Relang/src/org/reldb/relang/utilities/DialogBase.java
@@ -0,0 +1,30 @@
+package org.reldb.relang.utilities;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public abstract class DialogBase extends Dialog {
+	private Object result;
+
+	public DialogBase(Shell parent, int style) {
+		super(parent, style);
+	}
+
+	public abstract void create(Shell shell);
+	
+	public Object open() {
+		Shell parent = getParent();
+		Shell shell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		shell.setText(getText());
+		create(shell);
+		shell.open();
+		Display display = parent.getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		return result;
+	}
+}
