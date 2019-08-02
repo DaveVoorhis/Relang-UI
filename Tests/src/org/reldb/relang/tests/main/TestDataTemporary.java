f3c04f99e05999f02e99ce30f3090b5acf632a89
dave <dave@armchair.mb.ca>
2019-08-02 23:09:09 +0100

    Refactor to use Closeable interface.

--- Tests/src/org/reldb/relang/tests/main/TestDataTemporary.java
+++ Tests/src/org/reldb/relang/tests/main/TestDataTemporary.java
@@ -9,40 +9,42 @@ public class TestDataTemporary {
 	
 	@Test 
 	public void testDataTemporary01() {
-		var data = new DataTemporary();
-		data.setColumnName(0, "Column1");
-		data.setColumnName(1,  "Column2");
-		data.setColumnType(0, String.class, "");
-		data.setColumnType(1,  Integer.class, Integer.valueOf(0));
-		data.setValue(0, 1, "blah");
-		data.setValue(1,  1, 22);
-		data.setValue(1, 25, 33);
-		data.setValue(0,  44, "fish");
-		data.setColumnType(1, Object.class, Integer.valueOf(2));
-		assertEquals("blah", data.getValue(0,  1));
-		assertEquals(22, data.getValue(1, 1));
-		assertEquals(33, data.getValue(1, 25));
-		assertEquals("fish", data.getValue(0,  44));
-		data.appendDefaultColumn();
-		data.setValue(2, 2, "zot");
-		assertEquals("zot", data.getValue(2,  2));
-		data.appendDefaultColumn();
-		data.setValue(3, 100, "glub");
-		assertEquals("glub", data.getValue(3,  100));
+		try (var data = new DataTemporary()) {
+			data.setColumnName(0, "Column1");
+			data.setColumnName(1,  "Column2");
+			data.setColumnType(0, String.class, "");
+			data.setColumnType(1,  Integer.class, Integer.valueOf(0));
+			data.setValue(0, 1, "blah");
+			data.setValue(1,  1, 22);
+			data.setValue(1, 25, 33);
+			data.setValue(0,  44, "fish");
+			data.setColumnType(1, Object.class, Integer.valueOf(2));
+			assertEquals("blah", data.getValue(0,  1));
+			assertEquals(22, data.getValue(1, 1));
+			assertEquals(33, data.getValue(1, 25));
+			assertEquals("fish", data.getValue(0,  44));
+			data.appendDefaultColumn();
+			data.setValue(2, 2, "zot");
+			assertEquals("zot", data.getValue(2,  2));
+			data.appendDefaultColumn();
+			data.setValue(3, 100, "glub");
+			assertEquals("glub", data.getValue(3,  100));
+		}
 	}
 	
 	@Test
 	public void testDataTemporary02() {
-		var gridData = new DataTemporary();
-		gridData.setColumnName(0, "Col1");
-		gridData.setColumnName(1, "Col2");
-		gridData.setColumnName(2, "Col3");
-		gridData.appendRow();
-		gridData.appendRow();
-		gridData.appendRow();
-		gridData.appendRow();
-		assertEquals(3, gridData.getColumnCount());
-		assertEquals(4, gridData.getRowCount());
+		try (var gridData = new DataTemporary()) {
+			gridData.setColumnName(0, "Col1");
+			gridData.setColumnName(1, "Col2");
+			gridData.setColumnName(2, "Col3");
+			gridData.appendRow();
+			gridData.appendRow();
+			gridData.appendRow();
+			gridData.appendRow();
+			assertEquals(3, gridData.getColumnCount());
+			assertEquals(4, gridData.getRowCount());
+		}
 	}
 
 }
