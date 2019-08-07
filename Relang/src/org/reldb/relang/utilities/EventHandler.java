87c9e75782773c7668215065281b6ba865e6b852
Dave Voorhis <dave@armchair.mb.ca>
2019-08-07 22:45:53 +0100

    Generic event handling mechanisms.

---
+++ Relang/src/org/reldb/relang/utilities/EventHandler.java
@@ -0,0 +1,21 @@
+package org.reldb.relang.utilities;
+
+import java.util.Vector;
+
+public class EventHandler<Event> {
+
+	private Vector<EventListener<Event>> listeners = new Vector<>();
+	
+	public void addListener(EventListener<Event> listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(EventListener<Event> listener) {
+		listeners.remove(listener);
+	}
+
+	public void fire(Event event) {
+		listeners.forEach(listener -> listener.notify(event));
+	}
+
+}
