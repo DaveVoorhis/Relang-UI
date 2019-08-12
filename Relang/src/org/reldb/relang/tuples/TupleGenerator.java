9d6770ae503c5c79fe860d7d5dad953d0c1a6498
Dave Voorhis <dave@armchair.mb.ca>
2019-08-12 23:55:53 +0100

    Start on static, Java-based tuples.

---
+++ Relang/src/org/reldb/relang/tuples/TupleGenerator.java
@@ -0,0 +1,29 @@
+package org.reldb.relang.tuples;
+
+/**
+ * Generates Java code to represent a tuple, which is a class that implements Tuple.
+ * 
+ * @author dave
+ *
+ */
+public class TupleGenerator {
+	
+	public TupleGenerator(String dir, String tupleName) {
+		
+	}
+	
+	public boolean addAttribute(String name, Class type) {
+		
+		return true;
+	}
+	
+	public boolean removeAttribute(String name) {
+		
+		return true;
+	}
+	
+	public boolean compile() {
+		
+		return true;
+	}
+}
